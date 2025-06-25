3abb16c528538e79ac659a2a7e4a3bc4bed4f4c7
jetemi <mfadare95@gamil.com>
2025-06-25 23:54:19 +0100

    Scaffold from Wagtail

---
+++ pyconng/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure--5le$n8!#-0b9wd)bfx=w@tv8bhs2+%fa(6tu#nw*@xyv*d_mu"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
